b753d00a6eb9432fae2ec5011e1c1688466abf16
Teodosius Kenneth Louis Mariono <tmar647@aucklanduni.ac.nz>
2025-04-28 12:04:38 +0000

    Completed staging folder with all 8 regions

---
+++ models/staging/agent_report/carmen_sightings__asia/stg_carmen_sightings__asia.sql
@@ -0,0 +1,20 @@
+with carmen_sightings__asia as (
+    select *
+        from {{ source('carmen_sightings', 'carmen_sightings__asia') }}
+)
+
+select 
+    DATE_WITNESS,
+    DATE_AGENT,
+    WITNESS,
+    AGENT,
+    LATITUDE,
+    LONGITUDE,
+    CITY,
+    COUNTRY_CODE,
+    CITY_AGENT,
+    HAS_WEAPON,
+    HAS_HAT,
+    HAS_JACKET,
+    BEHAVIOR,
+    from carmen_sightings__asia
